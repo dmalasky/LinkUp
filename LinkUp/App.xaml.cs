db059c438238f71c52d7000690e4f57cc32b8dc4
dmalasky <dmalasky@byui.edu>
2023-10-20 17:43:40 -0600

    Add project files.

---
+++ LinkUp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkUp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LinkUp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkUp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
