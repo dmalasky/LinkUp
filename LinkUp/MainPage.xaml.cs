54b16fc183c8f368fb4cfdaf445fbe512ec11682
diazmoses <dia18003@byui.edu>
2023-10-21 02:44:01 -0600

    test

--- LinkUp/MainPage.xaml.cs
+++ LinkUp/MainPage.xaml.cs
@@ -1,15 +1,66 @@
-﻿using LinkUp.ViewModel;
+﻿using System;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Results;
+using OktaMauiSampleApp.Okta;
 
-namespace LinkUp
+namespace OktaMauiSampleApp;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    int count = 0;
+    private OktaClient _oktaClient;
+    private LoginResult _authenticationData;
+
+    public MainPage(OktaClient oktaClient)
     {
+        InitializeComponent();
+        _oktaClient = oktaClient;
+    }
 
-        public MainPage(MainViewModel vm)
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    public async void OnLoginClicked(object sender, EventArgs e)
+    {
+        var loginResult = await _oktaClient.LoginAsync();
+
+        if (!loginResult.IsError)
         {
-            InitializeComponent();
-            BindingContext = vm;
-        }
+            _authenticationData = loginResult;
+            LoginView.IsVisible = false;
+            HomeView.IsVisible = true;
 
+            UserInfoLvw.ItemsSource = loginResult.User.Claims;
+            HelloLbl.Text = $"Hello, {loginResult.User.Claims.FirstOrDefault(x => x.Type == "name")?.Value}";
+        }
+        else
+        {
+            await DisplayAlert("Error", loginResult.ErrorDescription, "OK");
+        }
+    }
+
+    public async void OnLogoutClicked(object sender, EventArgs e)
+    {
+        var logoutResult = await _oktaClient.LogoutAsync(_authenticationData.IdentityToken);
+
+        if (!logoutResult.IsError)
+        {
+            _authenticationData = null;
+            LoginView.IsVisible = true;
+            HomeView.IsVisible = false;
+        }
+        else
+        {
+            await DisplayAlert("Error", logoutResult.ErrorDescription, "OK");
+        }
     }
 }
