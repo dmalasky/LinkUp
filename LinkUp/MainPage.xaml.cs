28a1a376a047b243a0970348a6d8b876b4d21523
dmalasky <71410333+dmalasky@users.noreply.github.com>
2023-10-21 14:44:57 -0600

    Update MainPage.xaml.cs

--- LinkUp/MainPage.xaml.cs
+++ LinkUp/MainPage.xaml.cs
@@ -1,69 +1,19 @@
-﻿using System;
-using IdentityModel.OidcClient;
-using IdentityModel.OidcClient.Results;
-using LinkUp.Okta;
+using LinkUp.ViewModel;
 
-namespace LinkUp;
-
-public partial class MainPage : ContentPage
+namespace LinkUp
 {
-    int count = 0;
-    private OktaClient _oktaClient;
-    private LoginResult _authenticationData;
-
-    public MainPage(OktaClient oktaClient)
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
-        _oktaClient = oktaClient;
-    }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
-
-    public async void OnLoginClicked(object sender, EventArgs e)
-    {
-        var loginResult = await _oktaClient.LoginAsync();
-
-        if (!loginResult.IsError)
+        public MainPage(MainViewModel vm)
         {
-            _authenticationData = loginResult;
-            LoginView.IsVisible = false;
-            HomeView.IsVisible = true;
-
-            UserInfoLvw.ItemsSource = loginResult.User.Claims;
-            HelloLbl.Text = $"Hello, {loginResult.User.Claims.FirstOrDefault(x => x.Type == "name")?.Value}";
+            InitializeComponent();
+            BindingContext = vm;
         }
-        else
-        {
-            await DisplayAlert("Error", loginResult.ErrorDescription, "OK");
-        }
-    }
-
-    public async void OnLogoutClicked(object sender, EventArgs e)
-    {
-        var logoutResult = await _oktaClient.LogoutAsync(_authenticationData.IdentityToken);
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync(nameof(GroupCreationPage));
-        if (!logoutResult.IsError)
-        {
-            _authenticationData = null;
-            LoginView.IsVisible = true;
-            HomeView.IsVisible = false;
-        }
-        else
-        {
-            await DisplayAlert("Error", logoutResult.ErrorDescription, "OK");
         }
     }
 }
